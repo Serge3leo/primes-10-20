9af0b19a11c80ab0c1af63b2118b0f33d8e6f7e4
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2026-05-02 19:45:37 +0300

    sv-baseline/primes.py: minor change

--- sv-baseline/primes.py
+++ sv-baseline/primes.py
@@ -6,9 +6,8 @@ def main():
     n1 = max(2, n1)
     if n1 <= 2 < n2:
         print(2)
-    n1 += 1 - n1 % 2
-    assert n1 % 2 == 1
-    for n in range(n1, n2, 2):
+    n1 += 1 - n1 % 2              # make n1 odd
+    for n in range(n1, n2, 2):    # odd numbers only
         if all(n % d for d in range(3, math.isqrt(n) + 1, 2)):
             print(n)
 

7f08f5ffb3d7b61fe19b478734019beca1e85c0a
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2026-05-11 01:32:26 +0300

    pakuula-2/benchmark.py: simplification

--- pakuula-2/benchmark.py
+++ pakuula-2/benchmark.py
@@ -8,37 +8,11 @@ import numpy as np
 import benchmark_app
 
 
-def avg_time(n):
-    return 6.3 / 10000000
-
-
-def bsearch(low, high, pred):
-    if low >= high or pred(low):
-        return low
-    while low < high - 1:
-        mid = (low + high) // 2
-        if pred(mid):
-            high = mid
-        else:
-            low = mid
-    return high
-
-
-def find_dt(n):
-    v1 = avg_time(n)
-    return bsearch(
-        1,
-        3,
-        lambda dt: (avg_time(n + round(dt / v1)) - v1) / v1 > 0.01
-    )
-
-
 def make_scheme():
     scheme = collections.Counter()
+    s = round(3 / (6.3 / 10000000))
     for n1 in map(round, np.geomspace(1e6, 1e20, 1000)):
-        dt = find_dt(n1)
-        if dt >= 2:
-            scheme.update({(n1, n1 + round(dt / avg_time(n1))): 1})
+        scheme.update({(n1, n1 + s): 1})
     return scheme
 
 
